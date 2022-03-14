30c61011c5677f0b42542ca56a29582e634f6455
fredgit12 <101064607+fredgit12@users.noreply.github.com>
2022-03-14 19:58:24 +0100

    adding one more car

--- src/App.java
+++ src/App.java
@@ -3,6 +3,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         Car myFerrari = new Car("Ferrari","488"); 
         myFerrari.moveForward();
-        myFerrari.brake()
+        myFerrari.brake();
+            
+        Car myToyota = new Car("Toyota","corolla hybrid");
+        myToyota.turnLeft();
+        myToyota.moveForward();
     }
 }

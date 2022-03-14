a0fe8b5ae8b8aec57ec7f03871855487cb98b3fb
Zeeshan Sardar <zeeshansardar61@gmail.com>
2022-03-14 14:36:04 +0100

    Compilation error add on line number 5

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Car myFerrari = new Car("Ferrari","488"); 
-        myFerrari.moveForward();
+        myFerrari.moveForward()
         myFerrari.brake();
     }
 }
